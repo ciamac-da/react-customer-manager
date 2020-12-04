f61093a1c6a462455555aa80214a31bacfc61c82
ciamac-da <ciamac.davoudi@gmail.com>
2020-12-04 08:41:47 +0100

    verified by Cia v1.4.6 headers component

--- src/components/common/Headers.jsx
+++ src/components/common/Headers.jsx
@@ -1,1 +1,29 @@
 import React from 'react';
+import useStyles from '../../AppStyle';
+import {FcCustomerSupport} from "react-icons/fc";
+import { Typography } from '@material-ui/core';
+
+const Header = ({personsLenght}) => {
+    const classes = useStyles()
+    return (
+        <>
+             <Typography 
+             className={classes.h2}
+             component="h2"
+             >
+             Customer Manager 
+             <FcCustomerSupport />
+             </Typography>
+             <Typography 
+             className={classes.h3}
+             component="h3"
+             >
+             Number of customers  
+             &nbsp; 
+             <span className={classes.span}>{personsLenght}</span> 
+             </Typography>
+        </>
+      );
+}
+ 
+export default Header;
