cb58bf1d269d3560659c1bc36dce9bf0a9840ad9
ciamac-da <ciamac.davoudi@gmail.com>
2021-01-23 15:58:08 +0100

    Verified by Cia v2.0.2 reducer rexud

---
+++ src/reducers/persons.jsx
@@ -0,0 +1,14 @@
+export const personsReducer = (state= [], action)=>{
+    switch(action.type){
+        case "ADD_PERSON":
+            return [...action.payload]
+            case "UPDATE_PERSON":
+                return [...action.payload]
+                case "SAVE_PERSON":
+                    return [...action.payload]
+                    case "DELETE_PERSON":
+                        return [...action.payload]
+                        default:
+                            return state
+    }
+}
